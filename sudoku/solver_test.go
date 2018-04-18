bed9c747f32898125949e187037b4a9013f6f62d
Bruno Belarte <bruno.belarte@gmail.com>
2018-04-18 19:57:08 +0100

    Add Solver interface and recursive backtracking implementation

---
+++ sudoku/solver_test.go
@@ -0,0 +1,68 @@
+package sudoku_test
+
+import (
+	"testing"
+
+	"github.com/belarte/SuGoKu/sudoku"
+)
+
+var grid1 = sudoku.NewGrid("530070000" +
+	"600195000" +
+	"098000060" +
+	"800060003" +
+	"400803001" +
+	"700020006" +
+	"060000280" +
+	"000419005" +
+	"000080079")
+
+var grid1Solution = sudoku.NewGrid("534678912" +
+	"672195348" +
+	"198342567" +
+	"859761423" +
+	"426853791" +
+	"713924856" +
+	"961537284" +
+	"287419635" +
+	"345286179")
+
+var grid2 = sudoku.NewGrid("000000000" +
+	"000003085" +
+	"001020000" +
+	"000507000" +
+	"004000100" +
+	"090000000" +
+	"500000073" +
+	"002010000" +
+	"000040009")
+
+func TestRecursiveSolver(t *testing.T) {
+	var entries = []struct {
+		in       *sudoku.Grid
+		expected bool
+		out      *sudoku.Grid
+	}{
+		{grid1, true, grid1Solution},
+	}
+
+	var solver sudoku.RecursiveSolver
+	for _, entry := range entries {
+		if entry.expected != solver.Solve(entry.in) {
+			t.Errorf("\n%s\n\n%s", entry.in, entry.out)
+		}
+	}
+}
+
+func BenchmarkRecursiveGrid1(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		var solver sudoku.RecursiveSolver
+		solver.Solve(grid1)
+	}
+}
+
+func BenchmarkRecursiveGrid2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		var solver sudoku.RecursiveSolver
+		solver.Solve(grid2)
+	}
+}
